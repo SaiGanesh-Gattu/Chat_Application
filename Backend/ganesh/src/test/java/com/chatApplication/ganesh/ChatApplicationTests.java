7487acfd3ddd173526b1a0245a6f13d594a56b3f
Gattu Ganesh <gattu.ganesh@osbgroup.co.uk>
2025-08-27 22:50:14 +0530

    Backend Intialization of Chat Application using springboot

---
+++ Backend/ganesh/src/test/java/com/chatApplication/ganesh/ChatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chatApplication.ganesh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
--- Backend/src/services/authService.js
+++
@@ -1,8 +0,0 @@
-
-const signup=(req,res)=>{
-    res.send("Signup Successful");
-
-};
-
-
-export default signup;
